f4aada797f0dfdd71f517250b05545694f195b63
Josh Mountain <51127159+josh-mountain@users.noreply.github.com>
2023-03-25 00:44:04 +0000

    Update bitcoin_price.rs

--- bitcoin_price.rs
+++ bitcoin_price.rs
@@ -4,6 +4,22 @@ use serde_json::Value;
 use epd_waveshare::{epd2in13_v3::EPD2in13, color::Black, prelude::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // set up SPI pins and fonts
+    let mut spi = spidev::Spidev::open("/dev/spidev0.0")?;
+    let spi_options = spidev::SpidevOptions::new()
+        .max_speed_hz(4_000_000)
+        .mode(spidev::SpiModeFlags::SPI_MODE_0)
+        .build();
+    spi.configure(&spi_options)?;
+
+    let cs_pin = sysfs_gpio::Pin::new(8);
+    let busy_pin = sysfs_gpio::Pin::new(25);
+    let dc_pin = sysfs_gpio::Pin::new(24);
+    let rst_pin = sysfs_gpio::Pin::new(17);
+    let mut delay = std::thread::sleep;
+
+    let sans_font = include_bytes!("../fonts/DejaVuSans.ttf") as &[u8];
+
     // create a new instance of the display
     let mut epd = EPD2in13::new(&mut spi, cs_pin, busy_pin, dc_pin, rst_pin)?;
 
